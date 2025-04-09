1a89809e588b1babafaf4540ad287afc5189024d
Jesse Huang <jesse.huang@sifive.com>
2025-04-09 21:42:17 +0800

    Support ASUS TUF-AX4200Q

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -119,6 +119,7 @@ platform_do_upgrade() {
 		;;
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
+	asus,tuf-ax4200q|\
 	asus,tuf-ax6000|\
 	asus,zenwifi-bt8)
 		CI_UBIPART="UBI_DEV"
@@ -267,6 +268,7 @@ platform_pre_upgrade() {
 	case "$board" in
 	asus,rt-ax59u|\
 	asus,tuf-ax4200|\
+	asus,tuf-ax4200q|\
 	asus,tuf-ax6000|\
 	asus,zenwifi-bt8)
 		asus_initial_setup
